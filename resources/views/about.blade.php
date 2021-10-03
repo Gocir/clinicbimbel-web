ce99af0f486a3cc47ad1aac7d2ece9b7bdbcdc83
Ridwan Risandi <ridwanwanwan72@gmail.com>
2021-10-03 17:08:09 +0700

    Modify appearance to maintenance

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,88 +1,34 @@
-<!doctype html>
-<html lang="en">
+@extends('base.main')
 
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
-
-    <title>Clinic Bimbel</title>
-</head>
-
-<body>
-    {{-- Navbar --}}
-    <div class="container">
-        <nav class="navbar navbar-expand-lg navbar-light bg-transparant fs-5" style="height: 120px">
-            <div class="container">
-                <a class="navbar-brand mx-auto py-0" href="/">
-                    <img src="/img/Logo.png" class="py-4" style="max-height:120px" alt="">
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-                    <div class="nav navbar-nav">
-                        <a class="nav-link text-secondary me-3" aria-current="page" href="#">Home</a>
-                        <a class="nav-link text-secondary mx-3 active" href="#">About</a>
-                        <a class="nav-link text-secondary mx-3" href="#">Blog</a>
-                        <a class="nav-link text-success mx-3" href="#">Contact</a>
-                        <button type="button" class="btn btn-success btn-lg fs-6 ms-3" style="border-radius: 15px">Sign
-                            Up</button>
-                    </div>
-                </div>
-            </div>
-        </nav>
-    </div>
-    {{-- End of Navbar --}}
+@section('container')
     {{-- Jumbotron --}}
-    <section class="jumbotron">
-        <div class="container my-5">
-            <div class="row g-0">
-                <div class="col-5 my-auto">
-                    <div class="content mb-4">
-                        <p class="title">Oops!</p>
-                        <p class="sub-title">Under construction</p>
-                        <p>Website is still under construction, sorry for the inconvenience.
-                        </p>
+    <div class="container">
+        <section class="jumbotron">
+            <div class="row pt-4">
+                <div class="col-8 col-md-6 offset-md-3 col-lg-4 offset-lg-0 col-xl-4 offset-xl-0 my-auto">
+                    <div class="content">
+                        <h1 class="fw-bold m-auto display-4" style="font-size: 6vw">Oops!</h1>
+                        <h4 class="fw-bold mb-0" style="font-size: 2.2vw">Dalam Masa Pembangunan</h4>
+                        <h5 style="font-size: 1.2vw">Website masih dalam pembangunan, mohon maaf atas ketidaknyamanannya.
+                        </h5>
                     </div>
-                    {{-- <h1 class="fw-bold">Find the right Professional Mentor for you</h1>
-                    <p>Various topics, skill levels or languages, Join the Thousands of Learners in Indonesia Who Have Been
-                        Studying
-                        at the Clinic Bimbel.</p> --}}
-                    <a class="btn btn-success btn-lg" style="border-radius: 15px" href="#" role="button">More Info</a>
+                    {{-- <div class="cta">
+                        <a class="btn btn-success fs-5" href="/contact" role="button">Info lebih lanjut<i
+                                class="fas fa-chevron-right ms-2"></i></a>
+                    </div> --}}
+                    <div class="d-flex flex-row mt-5">
+                        <a href="https://www.facebook.com/clinicbimbel/" class="text-dark mx-3"><i
+                                class="fab fa-facebook-f"></i></a>
+                        <a href="https://www.instagram.com/clinicbimbel/" class="text-dark mx-1"><i
+                                class="fab fa-instagram"></i></a>
+                        <a href="http://wa.me/6283101065180" class="text-dark mx-3"><i class="fab fa-whatsapp"></i></a>
+                    </div>
                 </div>
-                <div class="col mx-4">
-                    <img src="/img/Under-construct-2x.png" class="img-fluid" alt="">
+                <div class="col-12 col-md-12 col-lg-8 col-xl-8 my-auto text-center">
+                    <img src="/img/under-construct.svg" class="img-fluid" alt="">
                 </div>
             </div>
-        </div>
-
-        {{-- <div class="row"></div> --}}
-    </section>
+        </section>
+    </div>
     {{-- End of Jumbotron --}}
-
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous">
-    </script>
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"
-integrity="sha384-eMNCOe7tC1doHpGoWe/6oMVemdAVTMs2xqW4mwXrXsW0L84Iytr2wi5v2QjrP/xp" crossorigin="anonymous">
-</script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.min.js"
-integrity="sha384-cn7l7gDp0eyniUwwAZgrzD06kc/tftFf19TOAs2zVinnD/C7E91j9yyk5//jjpt/" crossorigin="anonymous">
-</script>
--->
-</body>
-
-</html>
+@endsection
