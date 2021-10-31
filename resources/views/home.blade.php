a4c26612de498ff1a13fef41b9db1b1d25bc1be1
Ridwan Risandi <ridwanwanwan72@gmail.com>
2021-10-31 17:47:32 +0700

    Memperbarui tampilan halaman Home dan Responsibility

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,12 +4,12 @@
     {{-- Jumbotron --}}
     <section class="cover-home">
         <div class="jumbotron">
-            <div class="row">
-                <div class="col col-xl-4 m-auto">
+            <div class="row p-3 m-auto">
+                <div class="col-xl-5 col-md-5 order-2 order-sm-0 m-auto">
                     <div class="title-welcome text-light mb-4">
-                        <h1 class="fw-bold m-auto display-5">Temukan Pengajar Profesional yang tepat untukmu</h1>
-                        <h5>Kami hadir untuk mencerdaskan bangsa, menciptakan generasi yang edukatif
-                            dan kreatif.</h5>
+                        <h1 class="fw-bold m-auto display-6">Belajar bersama Pengajar Profesional yang tepat untukmu</h1>
+                        <h6>Kami hadir untuk mencerdaskan bangsa, menciptakan generasi yang edukatif
+                            dan kreatif.</h6>
                     </div>
                     <div class="cta">
                         <a class="btn btn-fb text-light me-lg-1" href="https://www.facebook.com/clinicbimbel/"
@@ -21,8 +21,8 @@
                             href="https://www.youtube.com/channel/UCuWJXJ1Tb1zewYcLOSQe5BQ" role="button">Youtube</a>
                     </div>
                 </div>
-                <div class="col col-xl-6 m-auto">
-                    <img src="/img/tentor.png" class="w-75 p-3" alt="">
+                <div class="col-xl-4 col-md-4 order-1 order-sm-0 m-auto">
+                    <img src="/img/tentor.png" class="w-100" alt="">
                 </div>
             </div>
         </div>
@@ -35,17 +35,24 @@
                         class="w-100"></iframe>
                 </div>
                 <div class="col-md-4">
-                    <h2 class="fw-bold mt-5 mt-md-0">Hubungi Kami</h2>
-                    <h6 class="mb-4">Punya beberapa saran atau hanya ingin menyapa? Tim dukungan kami siap
+                    <h2 class="fw-bold mt-5 mt-md-0 text-light">Hubungi Kami</h2>
+                    <h6 class="mb-4 text-light">Punya beberapa saran atau hanya ingin menyapa? Tim dukungan kami siap
                         membantu Anda 24/7.</h6>
                     <form>
-                        <input type="text" class="form-control my-3" id="inputName" placeholder="Nama Lengkap">
-                        <input type="email" class="form-control my-3" id="inputEmail" placeholder="nama@email.com">
-                        <input type="number" class="form-control my-3" id="inputPhone" placeholder="Nomor HP">
-                        <input type="text" class="form-control my-3" id="inputSubject" placeholder="Subjek">
-                        <textarea class="form-control my-3" aria-label="With textarea" rows="3"
-                            placeholder="Tambahkan pesan-mu disini .."></textarea>
-                        <button type="submit" class="btn btn-info text-light">Kirim</button>
+                        <div class="mb-3">
+                            <label for="exampleInputFullName" class="form-label text-light">Nama Lengkap</label>
+                            <input type="text" class="form-control" id="inputName" placeholder="Nama Lengkap">
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputEmail" class="form-label text-light">Email</label>
+                            <input type="email" class="form-control" id="inputEmail" placeholder="nama@email.com">
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputMessage" class="form-label text-light">Pesan</label>
+                            <textarea class="form-control" aria-label="With textarea" rows="3"
+                                placeholder="Tambahkan pesan-mu disini .."></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-action fw-bold">Kirim</button>
                     </form>
                 </div>
             </div>
